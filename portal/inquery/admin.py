caa6cce8b20caa57359bf9e9999b57493a2a48a6
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-10-11 14:00:35 +0500

    Приступаем к изучению FastAPI

--- portal/inquery/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-
-from inquery.models import Inquery, Client
-
-admin.site.register(Inquery)
-
-admin.site.register(Client)
--- portal/inquery/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-
-from inquery.views import DetailFrozeView, InqueryAPIView, ClientAPIView
-
-urlpatterns = [
-    path('', DetailFrozeView, name="calendar_today_url"),
-    path('api/v1/inquerylist/', InqueryAPIView.as_view(), name="inquerylist"),
-    path('api/v1/clientlist/', ClientAPIView.as_view(), name="clientlist"),
-]
