3615bed8209cc4fcdd21854427a0c3fd861e5706
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2022-10-23 00:21:33 +0500

    Запустили локальный сервер

--- app/main.py
+++ app/main.py
@@ -1,15 +1,16 @@
 from fastapi import FastAPI, APIRouter
 
 
-# Создали экземпляр объекта FastAPI app, который представляет собой класс Python, предоставляющий все функции для API.
+# 1. Создали экземпляр объекта FastAPI, который представляет собой класс Python, предоставляющий все функции для API.
 app = FastAPI(
     title="Recipe API", openapi_url="/openapi.json"
 )
 
-# 2
+# 2. Создали экземпляр APIRouter, с помощью которого сгруппировали наши конечные точки API.
 api_router = APIRouter()
 
-# 3
+
+# 3. Добавив @api_router.get("/", status_code=200) декоратор к функции root, определяем базовую точку GET для API.
 @api_router.get("/", status_code=200)
 def root() -> dict:
     """
@@ -17,11 +18,12 @@ def root() -> dict:
     """
     return {"msg": "Hello, World!"}
 
-# 4
+
+# 4. Используем метод include_router для регистрации маршрутизатора, созданного на шаге 2, в объекте FastAPI.
 app.include_router(api_router)
 
 
-# 5
+# 5. Если мы запускаем python app/main.py. Импортируем, uvicorn так как FastAPI зависит от этого веб-сервера.
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
