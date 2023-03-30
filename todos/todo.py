e3d8c21ba6d5e835c4a81b54e97227cd2c4a0d03
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-03-30 09:27:48 +0500

    Обработка ошибок

--- todos/todo.py
+++ todos/todo.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Path
+from fastapi import APIRouter, Path, HTTPException, status
 from model import Todo, TodoItem, TodoItems
 
 todo_router = APIRouter()
@@ -6,10 +6,12 @@ todo_router = APIRouter()
 todo_list = []
 
 
-@todo_router.post("/todo")
+@todo_router.post("/todo", status_code=201)
 async def add_todo(todo: Todo) -> dict:
     todo_list.append(todo)
-    return {"message": "Todo added successfully"}
+    return {
+        "message": "Todo added successfully"
+    }
 
 
 @todo_router.get("/todo", response_model=TodoItems)
@@ -26,9 +28,10 @@ async def get_single_todo(todo_id: int = Path(..., title="The ID of the todo to 
             return {
                 "todo": todo
             }
-    return {
-        "message": "Todo with supplied ID doesn't exist."
-    }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist",
+    )
 
 
 @todo_router.put("/todo/{todo_id}")
@@ -39,9 +42,11 @@ async def update_todo(todo_data: TodoItem, todo_id: int =Path(..., title="The ID
             return {
                 "message": "Todo updated successfully."
             }
-    return {
-        "message": "Todo with supplied ID doesn't exist."
-    }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist",
+    )
+
 
 @todo_router.delete("/todo/{todo_id}")
 async def delete_single_todo(todo_id: int) -> dict:
@@ -52,9 +57,10 @@ async def delete_single_todo(todo_id: int) -> dict:
             return {
                 "message": "Todo deleted successfully."
             }
-    return {
-        "message": "Todo with supplied ID doesn't exist."
-    }
+    raise HTTPException(
+        status_code=status.HTTP_404_NOT_FOUND,
+        detail="Todo with supplied ID doesn't exist",
+    )
 
 
 @todo_router.delete("/todo")
