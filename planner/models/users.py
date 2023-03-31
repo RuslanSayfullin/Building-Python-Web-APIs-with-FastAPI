eb93d6c6d4cb8ac5a75a44e600b14aba38cbce22
Ruslan Sayfullin <ruslansaifullin91@gmail.com>
2023-03-31 10:15:50 +0500

    Реализация моделей приложения для планирования мероприятий

---
+++ planner/models/users.py
@@ -0,0 +1,33 @@
+from pydantic import BaseModel, EmailStr
+from typing import Optional, List
+
+from planner.models.events import Event
+
+
+class User(BaseModel):
+    email: EmailStr
+    password: str
+    events: Optional[List[Event]]
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "username": "strong!!!",
+                "events": [],
+            }
+        }
+
+
+class UserSignIn(BaseModel):
+    email: EmailStr
+    password: str
+
+    class Config:
+        schema_extra = {
+            "example": {
+                "email": "fastapi@packt.com",
+                "password": "strong!!!",
+                "events": [],
+            }
+        }
--- todos/model.py
+++ todos/model.py
@@ -1,16 +1,24 @@
-from typing import List
+from typing import List, Optional
+from fastapi import Form
 from pydantic import BaseModel
 
 
 class Todo(BaseModel):
-    id: int
+    id: Optional[int]
     item: str
+
+    @classmethod
+    def as_form(
+            cls,
+            item: str = Form(...)
+    ):
+        return cls(item=item)
 
     class Config:
         schema_extra = {
             "example": {
                 "id": 1,
-                "item": "Example Schema!"
+                "item": "Example schema!"
             }
         }
 
